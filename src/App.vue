db8592b5f6ec7f0da9f513f2fb9d045d15862ca1
moronq <moronqq@gmail.com>
2022-09-12 17:22:54 +0300

    infinite pagination

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
-    <my-button @click="openModal">Создать пост</my-button>
+    <my-input v-model="search" placeholder="search..." />
+    <div class="app__btns">
+      <my-button @click="openModal">Создать пост</my-button>
+      <my-select v-model="selectedSort" :options="sortOptions" />
+    </div>
     <my-dialog v-model:show="dialogVisible">
       <post-form @create="createPost" />
     </my-dialog>
-    <post-list :posts="posts" @remove="removePost" v-if="!isLoading" />
+    <post-list
+      :posts="searchSortedPost"
+      @remove="removePost"
+      v-if="!isLoading"
+    />
     <div v-else>Идет загрузка...</div>
+    <div ref="observer" class="observer"></div>
+    <!-- <div class="page__wrapper">
+      <div
+        v-for="pageNumber in totalPages"
+        :key="pageNumber"
+        class="page"
+        :class="{
+          'current-page': page === pageNumber,
+        }"
+        @click="changePage(pageNumber)"
+      >
+        {{ pageNumber }}
+      </div>
+    </div> -->
   </div>
 </template>
 
@@ -16,18 +38,31 @@ import PostForm from '@/components/PostForm.vue'
 import MyDialog from './components/UI/MyDialog.vue'
 import axios from 'axios'
 import MyButton from './components/UI/MyButton.vue'
+import MySelect from './components/UI/MySelect.vue'
+import MyInput from './components/UI/MyInput.vue'
 export default {
   components: {
     PostList,
     PostForm,
     MyDialog,
     MyButton,
+    MySelect,
+    MyInput,
   },
   data() {
     return {
       posts: [],
       dialogVisible: false,
       isLoading: false,
+      selectedSort: '',
+      sortOptions: [
+        { value: 'title', name: 'По названию' },
+        { value: 'body', name: 'По содержимому' },
+      ],
+      search: '',
+      page: 1,
+      limit: 10,
+      totalPages: 0,
     }
   },
   methods: {
@@ -41,12 +76,24 @@ export default {
     openModal() {
       this.dialogVisible = true
     },
+    // changePage(pageNumber) {
+    //   this.page = pageNumber
+    // },
     async fetchPosts() {
       try {
         this.isLoading = true
         setTimeout(async () => {
           const response = await axios.get(
-            'https://jsonplaceholder.typicode.com/posts?_limit=10'
+            'https://jsonplaceholder.typicode.com/posts?',
+            {
+              params: {
+                _page: this.page,
+                _limit: this.limit,
+              },
+            }
+          )
+          this.totalPages = Math.ceil(
+            response.headers['x-total-count'] / this.limit
           )
           this.posts = response.data
           this.isLoading = false
@@ -56,9 +103,56 @@ export default {
         alert('Ошибка')
       }
     },
+    async loadMorePosts() {
+      try {
+        this.page += 1
+        setTimeout(async () => {
+          const response = await axios.get(
+            'https://jsonplaceholder.typicode.com/posts?',
+            {
+              params: {
+                _page: this.page,
+                _limit: this.limit,
+              },
+            }
+          )
+          this.totalPages = Math.ceil(
+            response.headers['x-total-count'] / this.limit
+          )
+          this.posts = [...this.posts, ...response.data]
+        }, 1000)
+      } catch (e) {
+        alert('Ошибка')
+      }
+    },
   },
   mounted() {
     this.fetchPosts()
+    const options = { rootMargin: '0px', treshold: 1.0 }
+    const callback = (entries, observer) => {
+      if (entries[0].isIntersecting && this.page < this.totalPages) {
+        this.loadMorePosts()
+      }
+    }
+    const observer = new IntersectionObserver(callback, options)
+    observer.observe(this.$refs.observer)
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((part1, part2) => {
+        return part1[this.selectedSort]?.localeCompare(part2[this.selectedSort])
+      })
+    },
+    searchSortedPost() {
+      return this.sortedPosts.filter((post) => {
+        return post.title.toLowerCase().includes(this.search.toLowerCase())
+      })
+    },
+  },
+  watch: {
+    // page() {
+    //   this.fetchPosts()
+    // },
   },
 }
 </script>
@@ -72,5 +166,23 @@ export default {
 }
 .app {
   padding: 20px;
+}
+.app__btns {
+  display: flex;
+  justify-content: space-between;
+  margin: 15px 0;
+}
+.page__wrapper {
+  display: flex;
+  margin-top: 15px;
+  gap: 10px;
+}
+.page {
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 5px 10px;
+}
+.current-page {
+  border: 2px solid teal;
 }
 </style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,20 +1,23 @@
 <template>
   <h3>Список постов</h3>
   <ul v-if="posts.length > 0">
-    <post-item
-      v-for="post in posts"
-      :post="post"
-      :key="post.id"
-      @remove="$emit('remove', post)"
-    />
+    <transition-group name="list">
+      <post-item
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </ul>
   <h4 v-else>Список постов пуст</h4>
 </template>
 
 <script>
 import PostItem from './PostItem.vue'
+import MyInput from './UI/MyInput.vue'
 export default {
-  components: { PostItem },
+  components: { PostItem, MyInput },
   props: {
     posts: {
       type: Array,
@@ -25,4 +28,21 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.2s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.list-move {
+  transition: transform 0.8s ease;
+}
+</style>
